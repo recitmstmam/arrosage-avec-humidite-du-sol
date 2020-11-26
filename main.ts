45b45cfd255b8d97da4ac823a7e3ee7e2cef4b95
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-11-26 12:00:51 -0500

    Mise à jour main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P7, 1)
+    pins.analogWritePin(AnalogPin.P4, 1023)
     basic.pause(100)
     reading = pins.analogReadPin(AnalogPin.P3)
     basic.pause(100)
-    pins.digitalWritePin(DigitalPin.P7, 0)
-    pourcentage = Math.round((reading - 480) * 100 / (1023 - 480))
+    pins.analogWritePin(AnalogPin.P4, 0)
+    pourcentage = Math.round((reading - 610) * 100 / (1023 - 610))
     basic.showString("" + pourcentage + "%")
 })
 let pourcentage = 0
@@ -15,12 +15,12 @@ servos.P2.setAngle(0)
 basic.pause(1000)
 servos.P2.stop()
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P7, 1)
+    pins.analogWritePin(AnalogPin.P4, 1023)
     basic.pause(100)
     reading = pins.analogReadPin(AnalogPin.P3)
     basic.pause(100)
-    pins.digitalWritePin(DigitalPin.P7, 0)
-    pourcentage = Math.round((reading - 480) * 100 / (1023 - 480))
+    pins.analogWritePin(AnalogPin.P4, 0)
+    pourcentage = Math.round((reading - 610) * 100 / (1023 - 610))
     if (pourcentage <= 50) {
         servos.P2.setAngle(180)
     } else if (pourcentage >= 50 && pourcentage <= 80) {
