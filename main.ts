7957f15cb524952e40793fcadcab2a4c15a6bc6d
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-12-08 07:58:16 -0500

    Mise à jour main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ input.onButtonPressed(Button.B, function () {
     reading = pins.analogReadPin(AnalogPin.P3)
     basic.pause(100)
     pins.digitalWritePin(DigitalPin.P0, 0)
-    pourcentage = Math.round((reading - 610) * 100 / (1023 - 610))
+    pourcentage = Math.round((reading - 600) * 100 / (1023 - 600))
     basic.showString("" + pourcentage + "%")
 })
 let pourcentage = 0
@@ -20,7 +20,7 @@ basic.forever(function () {
     reading = pins.analogReadPin(AnalogPin.P3)
     basic.pause(100)
     pins.digitalWritePin(DigitalPin.P7, 0)
-    pourcentage = Math.round((reading - 610) * 100 / (1023 - 610))
+    pourcentage = Math.round((reading - 600) * 100 / (1023 - 600))
     if (pourcentage <= 95) {
         servos.P2.setAngle(180)
     } else if (pourcentage >= 95 && pourcentage <= 98) {
@@ -28,7 +28,7 @@ basic.forever(function () {
     } else if (pourcentage > 98) {
         servos.P2.setAngle(0)
     }
-    radio.sendValue("H sol", pourcentage)
+    radio.sendValue("sol", pourcentage)
     basic.pause(1000)
     servos.P2.stop()
     // 30 secondes avant la prochaine mesure.
